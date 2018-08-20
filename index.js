97a880c1b6f43a59162a802afd8716c334b976d3
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-08-20 17:43:36 -0400

    not sure what the => syntax is...

---
+++ index.js
@@ -0,0 +1,65 @@
+let store = { drivers: [], passengers: [], trips: [] }
+
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId
+    this.name = name
+    store.drivers.push(this)
+  }
+  trips(){
+    return store.trips.filter(trip => {
+      return trip.driverId === this.id
+    })
+  }
+  passengers(){
+    return store.passengers.filter(passenger => {
+      return passenger.tripId === this.tripId
+    })
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+  }
+  trips(){
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id
+    })
+  }
+  drivers(){
+    return store.drivers.filter(driver => {
+      return driver.tripId === this.tripId
+    })
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    this.name = name
+    if (driver) {
+      this.driverId = driver.id
+    }
+    if (passenger) {
+      this.passengerId = passenger.id
+    }
+    store.trips.push(this)
+  }
+  driver(){
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId
+    });
+  }
+  passenger(){
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId
+    });
+  }
+}
